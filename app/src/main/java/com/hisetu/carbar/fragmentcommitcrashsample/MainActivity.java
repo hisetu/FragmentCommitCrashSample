43eabff1b89e8792e5b253d8fbc2c7d4cb9735d0
lucas <lucas@rolymobile.com>
2015-12-01 15:30:02 +0800

    ok!

--- app/src/main/java/com/hisetu/carbar/fragmentcommitcrashsample/MainActivity.java
+++ app/src/main/java/com/hisetu/carbar/fragmentcommitcrashsample/MainActivity.java
@@ -1,18 +1,14 @@
 package com.hisetu.carbar.fragmentcommitcrashsample;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Handler handler;
+public class MainActivity extends SafeAppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +26,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        handler = new Handler();
-        handler.post(new Runnable() {
+        new Thread(new Runnable() {
+            private int num = 1;
+
             @Override
             public void run() {
-                AlertDialogSupportFragment.newInstance(R.string.app_name, R.string.app_name)
-                        .show(getSupportFragmentManager(), "");
-                handler.postDelayed(this, 5000);
+                while (true) {
+                    showDialog();
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }
-        });
+
+            private void showDialog() {
+                safeCommit(new Runnable() {
+                    @Override
+                    public void run() {
+                        AlertDialogSupportFragment.newInstance(R.string.app_name, R.string.app_name)
+                                .show(getSupportFragmentManager(), "");
+                    }
+                });
+            }
+        }, "show dialog").start();
     }
 
     @Override
